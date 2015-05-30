80ec4558e11daa463856f02e682f34f75c025b6f
danielEXAMEN <danielEXAMEN@gmail.com>
2015-05-30 23:38:05 +0200

    Actualizacion 6
    Creacion de 3 instancias
    
    de las clases clientes,productos,vendedores y tiendas.

--- TiendaDaniel003/src/Tienda/principal.java
+++ TiendaDaniel003/src/Tienda/principal.java
@@ -3,7 +3,18 @@ package Tienda;
 public class principal {
 
 	public static void main(String[] args) {
-		
+		clientes cliente1= new clientes("clienteuno");
+		clientes cliente2= new clientes("clientedos");
+		clientes cliente3= new clientes("clientetres");
+		productos producto1=new productos("productouno");
+		productos producto2=new productos("productodos");
+		productos producto3=new productos("productotres");
+		vendedores vendedor1=new vendedores("vendedoruno");
+		vendedores vendedor2=new vendedores("vendedordos");
+		vendedores vendedor3=new vendedores("vendedortres");
+		tiendas tienda1=new tiendas("tiendauno");
+		tiendas tienda2=new tiendas("tiendados");
+		tiendas tienda3=new tiendas("tiendatres");
 	}
 
 }
